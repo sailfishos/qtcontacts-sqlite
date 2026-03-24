658dd0fbc70620b8cfbbcfd8168967f06af6ef6a
Adriaan de Groot <groot@kde.org>
2026-03-24 16:40:49 +0100

    Qt6: use QRecursiveMutex
    (cherry picked from commit 62cdfe0bfbf2f7f25c093485a3d15cd9989a0384)
    (cherry picked from commit 7e1d3e4797bc0ccd2fc098079dc9629455bfa21b)
    
    [Henrik Schnor: add version guards to keep compatibility with Qt 5.6]

--- src/engine/contactstransientstore.cpp
+++ src/engine/contactstransientstore.cpp
@@ -76,7 +76,9 @@ public:
     typedef std::tr1::function<void ()> Function;
 
     SharedMemoryManager()
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
         : m_mutex(QMutex::Recursive)
+#endif
     {
     }
 
@@ -186,7 +188,11 @@ private:
 
     QMap<QString, TableData> m_tables;
     QScopedPointer<Semaphore> m_semaphore;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    QRecursiveMutex m_mutex;
+#else
     QMutex m_mutex;
+#endif
 };
 
 Q_GLOBAL_STATIC(SharedMemoryManager, sharedMemory);
