297fb17d64e083e95ece7c087c65efe5b6532d16
Adriaan de Groot <groot@kde.org>
2026-03-24 13:54:19 +0100

    Qt6: adjust mutex type
    (cherry picked from commit af9822d7bca686cfed9b0f0aa0b37a44fadf223f)
    
    [Henrik Schnor: add typedef with version guard for Qt 5.6 compatibility]

--- src/engine/contactsengine.cpp
+++ src/engine/contactsengine.cpp
@@ -944,10 +944,16 @@ private:
 
 class JobThread : public QThread
 {
-    struct MutexUnlocker {
-        QMutexLocker &m_locker;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    typedef QMutexLocker<QMutex> MutexLocker;
+#else
+    typedef QMutexLocker MutexLocker;
+#endif
 
-        explicit MutexUnlocker(QMutexLocker &locker)
+    struct MutexUnlocker {
+        MutexLocker &m_locker;
+
+        explicit MutexUnlocker(MutexLocker &locker)
             : m_locker(locker)
         {
             m_locker.unlock();
@@ -1234,7 +1240,7 @@ void JobThread::run()
     QString dbId(QStringLiteral("qtcontacts-sqlite%1-job-%2"));
     dbId = dbId.arg(m_autoTest ? QStringLiteral("-test") : QString()).arg(m_databaseUuid);
 
-    QMutexLocker locker(&m_mutex);
+    MutexLocker locker(&m_mutex);
 
     m_database.open(dbId, m_nonprivileged, m_autoTest);
     m_nonprivileged = m_database.nonprivileged();
