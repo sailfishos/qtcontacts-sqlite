1a7c641f2578ca5d5b17584a2077788b30e001e5
Henrik Schnor <hschnor@mailbox.org>
2026-08-27 15:03:18 +0200

    Qt6: fix for QContactManagerEngine api change
    QContactManagerEngine::collection() and
    QContactManagerEngine::collections() are now non-const in Qt 6.

--- src/engine/contactsengine.cpp
+++ src/engine/contactsengine.cpp
@@ -1606,7 +1606,10 @@ QContactCollectionId ContactsEngine::defaultCollectionId() const
 
 QContactCollection ContactsEngine::collection(
         const QContactCollectionId &collectionId,
-        QContactManager::Error *error) const
+        QContactManager::Error *error)
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+        const
+#endif
 {
     const QList<QContactCollection> collections = ContactsEngine::collections(error);
 
@@ -1623,7 +1626,10 @@ QContactCollection ContactsEngine::collection(
 }
 
 QList<QContactCollection> ContactsEngine::collections(
-        QContactManager::Error *error) const
+        QContactManager::Error *error)
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+        const
+#endif
 {
     QList<QContactCollection> collections;
 
